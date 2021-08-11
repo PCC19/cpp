a7439ac9808ef0af0df2cdbe7b82aead12ee09ed
Paulo Cunha <pcunha00@hotmail.com>
2021-08-11 17:07:42 +0200

    ex00

---
+++ Module 00/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcunha <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/11 16:28:52 by pcunha            #+#    #+#             */
+/*   Updated: 2021/08/11 17:04:50 by pcunha           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		i = 1;
+		while (argv[i] != 0)
+		{
+			while (*argv[i] != 0)
+			{
+				*argv[i] = toupper(*argv[i]);
+				std::cout << *argv[i];
+				argv[i]++;
+			}
+			argv++;
+		}
+		std::cout << '\n';
+	}
+	return (0);
+}
