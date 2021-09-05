990b18ff0d194ccc0d5efe78a4a2722ba6c05099
Paulo Cunha <pcunha00@hotmail.com>
2021-09-05 22:46:55 +0200

    fixed comments on mod7ex02

--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pcunha <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 20:57:24 by pcunha            #+#    #+#             */
-/*   Updated: 2021/08/26 20:57:26 by pcunha           ###   ########.fr       */
+/*   Updated: 2021/09/05 22:46:07 by pcunha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,60 +58,5 @@ int main(int, char**)
 		{
 			std::cout << a[i] << " " << a[i].length() << std::endl;
 		}
-	{
-	return (0);
-}
-//
-//    Array<int> numbers(MAX_VAL);
-//    int* mirror = new int[MAX_VAL];
-//    srand(time(NULL));
-//    for (int i = 0; i < MAX_VAL; i++)
-//    {
-//        const int value = rand();
-//        numbers[i] = value;
-//        mirror[i] = value;
-//        std::cout << numbers[i] << std::endl;
-//        std::cout << mirror[i] << "\n" << std::endl;
-//    }
-//    //SCOPE
-//    {
-//        Array<int> tmp = numbers;
-//        Array<int> test(tmp);
-//        std::cout << "***" << std::endl;
-//        std::cout << tmp[5] << std::endl;
-//        std::cout << test[5] << "\n***" << std::endl;
-//    }
-//
-//    for (int i = 0; i < MAX_VAL; i++)
-//    {
-//        if (mirror[i] != numbers[i])
-//        {
-//            std::cerr << "didn't save the same value!!" << std::endl;
-//            return 1;
-//        }
-//    }
-//    try
-//    {
-//        numbers[-2] = 0;
-//    }
-//    catch(const std::exception& e)
-//    {
-//        std::cerr << e.what() << '\n';
-//    }
-//    try
-//    {
-//        numbers[MAX_VAL] = 0;
-//    }
-//    catch(const std::exception& e)
-//    {
-//        std::cerr << e.what() << '\n';
-//    }
-//    std::cout << std::endl;
-//    for (int i = 0; i < MAX_VAL; i++)
-//    {
-//        numbers[i] = rand();
-//        std::cout << numbers[i] << "\n" << std::endl;
-//    }
-//    delete [] mirror;
     return 0;
 }
